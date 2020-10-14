12d4f29a59c55deb4add394f3bb2f81062bcbe95
arais <xero0001@naver.com>
2020-10-15 03:36:49 +0900

    Project Copy

--- examples/09_vector_class_and_operator_overloading/ECS/Components.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef Components_hpp
-#define Components_hpp
-
-#include "ECS.hpp"
-#include "TransformComponent.hpp"
-#include "SpriteComponent.hpp"
-#include "../TextureManager.hpp"
-
-#endif /* Components_hpp */
--- examples/14_render_layers_and_grouping/ECS.cpp
+++
@@ -1,7 +0,0 @@
-#include "ECS/ECS.hpp"
-
-void Entity::addGroup(Group mGroup)
-{
-  groupBitset[mGroup] = true;
-  manager.AddToGroup(this, mGroup);
-}
